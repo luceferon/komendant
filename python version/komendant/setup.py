8c240f5dba3cb2d8ba4835ed7f209448b7ec713a
Denis Vladimirovich <luceferon3@gmail.com>
2024-05-09 08:46:34 +0700

    откат переноса

---
+++ python version/komendant/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='komendant',
+    version='1',
+    packages=[''],
+    url='',
+    license='',
+    author='Листопад Денис',
+    author_email='',
+    description=''
+)
